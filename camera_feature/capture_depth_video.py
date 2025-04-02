b6c6dbb43c8ec8ca260a83b9134f07680ec09e8b
Luckey Sherpa <luckeysherpa07@gmail.com>
2025-04-02 16:10:54 +0200

    removed unncessary file naming capture depth video

--- camera_feature/capture_depth_video.py
+++
@@ -1,7 +0,0 @@
-import pyzed.sl as sl
-
-def run():
-    print("Yet to code")
-
-if __name__ == "__main__":
-    run()
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from camera_feature import camera_calibration
 from camera_feature import zed_timestamp
 from camera_feature import video_capture
-from camera_feature import capture_depth_video
 from camera_feature import video_playback
 from camera_feature import capture_fps
 from camera_feature import display_depth_video
@@ -11,30 +10,27 @@ def main():
     print("Choose a function to run:")
     print("1. Display Camera Calibration")
     print("2. Capture Video")
-    print("3. Capture Depth Video")
-    print("4. Display ZED Timestamp")
-    print("5. Playback Video")
-    print("6. Display Depth Video")
-    print("7. Display FPS")
-    print("8. Align Video")
+    print("3. Display ZED Timestamp")
+    print("4. Playback Video")
+    print("5. Display Depth Video")
+    print("6. Display FPS")
+    print("7. Align Video")
 
-    choice = input("Enter the number (1/2/3/4/5/6/7/8): ")
+    choice = input("Enter the number (1/2/3/4/5/6/7): ")
 
     if choice == "1":
         camera_calibration.run()
     elif choice == "2":
         video_capture.run()
     elif choice == "3":
-        capture_depth_video.run()
-    elif choice == "4":
         zed_timestamp.run()
-    elif choice == "5":
+    elif choice == "4":
         video_playback.run()
-    elif choice == "6":
+    elif choice == "5":
         display_depth_video.run()
-    elif choice == "7":
+    elif choice == "6":
         capture_fps.run()
-    elif choice == "8":
+    elif choice == "7":
         align_video.run()
     else:
         print("Invalid choice!")
