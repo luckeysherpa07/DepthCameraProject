4fa67fc38808bf861e6ca7f9527fa858c46082a4
Luckey Sherpa <luckeysherpa07@gmail.com>
2025-04-15 15:37:17 +0200

    added empty file for simultaneous showing of deptshowing of depth and confidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from camera_feature import camera_calibration
 from camera_feature import display_live_feed  # Now option 2
+from camera_feature import depth_confidence_playback  # New option 3
 from camera_feature import zed_timestamp
 from camera_feature import video_playback
 from camera_feature import capture_fps
@@ -12,33 +13,36 @@ def main():
     print("Choose a function to run:")
     print("1. Display Camera Calibration")
     print("2. Display Live Feed")
-    print("3. Display ZED Timestamp")
-    print("4. Playback Video")
-    print("5. Display FPS")
-    print("6. Display Depth Video")
-    print("7. Display Confidence Map")
-    print("8. Capture Video")
-    print("9. Align Video")
+    print("3. Depth and Confidence Playback")
+    print("4. Display ZED Timestamp")
+    print("5. Playback Video")
+    print("6. Display FPS")
+    print("7. Display Depth Video")
+    print("8. Display Confidence Map")
+    print("9. Capture Video")
+    print("10. Align Video")
 
-    choice = input("Enter the number (1-9): ")
+    choice = input("Enter the number (1-10): ")
 
     if choice == "1":
         camera_calibration.run()
     elif choice == "2":
         display_live_feed.run()
     elif choice == "3":
-        zed_timestamp.run()
+        depth_confidence_playback.run()
     elif choice == "4":
-        video_playback.run()
+        zed_timestamp.run()
     elif choice == "5":
-        capture_fps.run()
+        video_playback.run()
     elif choice == "6":
-        display_depth_video.run()
+        capture_fps.run()
     elif choice == "7":
-        display_confidence_map.run()
+        display_depth_video.run()
     elif choice == "8":
-        video_capture.run()
+        display_confidence_map.run()
     elif choice == "9":
+        video_capture.run()
+    elif choice == "10":
         align_video.run()
     else:
         print("Invalid choice!")
